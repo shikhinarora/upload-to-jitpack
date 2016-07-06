72473e52069a10246a328a7502c28157e666fbcf
shikhin <shikhin.arora@gmail.com>
2016-07-06 13:55:26 +0530

    second

--- app/src/main/java/uploadtojitpack/demo/shikhin/jitpack/MainActivity.java
+++ app/src/main/java/uploadtojitpack/demo/shikhin/jitpack/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TestLibrary.getInstance().makeMeAwesome(this, "shikhin");
+        TestLibrary.getInstance().makeMeAwesome(this, "shikhin");
+        //comment after first commit
     }
 }
